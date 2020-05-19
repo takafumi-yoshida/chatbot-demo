c550b41ebf7568e4e4608353fce832b3ee37bb65
takafumi-yoshida <59679612+takafumi-yoshida@users.noreply.github.com>
2020-05-20 00:49:31 +0900

    update(App)

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import './App.css';
+import './assets/styles/style.css';
+import defaultDataset from "./dataset"
 
-export default class App extends React.Component() {
-
+export default class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       answers: [],            //回答コンポーネントに表示するデータ
       chats: [],              //チャットコンポーネントに表示するデータ
       currentId: "init",      // 現在の質問ID
-      dataset: {},            //質問と回答のデータセット
+      dataset: defaultDataset,            //質問と回答のデータセット
       open: false             //お問い合わせフォーム用のモーダルの開閉を管理
     }
   }
   render() {
     return (
       <div>
+        <section className="c-section">
+          <div className="c-box">
 
+          </div>
+        </section>
       </div>
     );
   }
