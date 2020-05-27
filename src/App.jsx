4081ec518c63c172ca6248d7ab6d6f630b0e04f6
takafumi-yoshida <59679612+takafumi-yoshida@users.noreply.github.com>
2020-05-27 16:50:01 +0900

    create (Inquiry form)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './assets/styles/style.css';
 import defaultDataset from "./dataset";
 import {AnswersList, Chats} from "./components/index"
+import FormDialog from './components/Forms/FormDialog';
 
 export default class App extends React.Component {
 
@@ -15,6 +16,8 @@ export default class App extends React.Component {
       open: false             //お問い合わせフォーム用のモーダルの開閉を管理
     }
     this.selectedAnswer = this.selectAnswer.bind(this)
+    this.handleClickOpen = this.handleClickOpen.bind(this)
+    this.handleClose = this.handleClose.bind(this)
   }
 
   displayNextQuestion = (nextQuestionId) => {
@@ -36,6 +39,10 @@ export default class App extends React.Component {
       case (nextQuestionId === 'init'):
         this.displayNextQuestion(nextQuestionId)
         break;
+      case (nextQuestionId === 'contact'):
+        this.handleClickOpen()
+        break;
+        
       default:
         const chat = {
           text: selectedAnswer,
@@ -62,6 +69,14 @@ export default class App extends React.Component {
     }
   }
 
+  handleClickOpen = () => {
+    this.setState({open: true});
+  };
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
+
   render() {
     return (
       <div>
@@ -69,6 +84,7 @@ export default class App extends React.Component {
           <div className="c-box">
             <Chats chats={this.state.chats}/>
             <AnswersList answers={this.state.answers} select={this.selectAnswer}/>
+            <FormDialog open={this.state.open} handleClose={this.handleClose}/>
           </div>
         </section>
       </div>
---
+++ src/components/Forms/FormDialog.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import InputText from './InputText'
+
+export default class FormDialog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      discription:""
+    }
+    this.inputName = this.inputName.bind(this)
+    this.inputEmail = this.inputEmail.bind(this)
+    this.inputDiscription = this.inputDiscription.bind(this)
+  }
+
+  inputName = (event) => {
+    this.setState({name: event.target.value})
+  }
+  inputEmail = (event) => {
+    this.setState({email: event.target.value})
+  }
+  inputDiscription = (event) => {
+    this.setState({discription: event.target.value})
+  }
+
+
+  render() {
+    return(
+      <Dialog
+        open={this.props.open}
+        onClose={this.props.handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"お問い合わせフォーム"}</DialogTitle>
+        <DialogContent>
+          <InputText 
+            label={"お名前{必須}"}
+            multiline={false}
+            rows={1}
+            value={this.state.name}
+            type={"text"}
+            onChange={this.inputName}
+          />
+          <InputText 
+            label={"メールアドレス{必須}"}
+            multiline={false}
+            rows={1}
+            value={this.state.email}
+            type={"email"}
+            onChange={this.inputEmail}
+          />
+          <InputText 
+            label={"お問い合わせ内容{必須}"}
+            multiline={true}
+            rows={5}
+            value={this.state.description}
+            type={"text"}
+            onChange={this.inputDescription}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.props.handleClose} color="primary">
+            Disagree
+          </Button>
+          <Button onClick={this.props.handleClose} color="primary" autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    )
+  }
+}
